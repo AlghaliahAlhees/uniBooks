aca5c2ed91e4a78fcf12af234f04a66cbdd5fb76
Alghalya fahad <44530081+iAgf@users.noreply.github.com>
2023-02-04 15:09:30 +0300

    Initial Commit

---
+++ uniBooks/uniBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  uniBooksApp.swift
+//  uniBooks
+//
+//  Created by Alghalya Alhees on 04/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct uniBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
